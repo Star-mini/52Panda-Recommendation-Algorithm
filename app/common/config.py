2972abbe9df74acaf97b18ad0aa63c0ae2a27f51
Sang Min Park <minisstar@naver.com>
2024-05-20 16:21:22 +0900

    feat:디비 초기 세팅

--- app/common/config.py
+++ app/common/config.py
@@ -1,4 +1,8 @@
+import os
+
 class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///your_database.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://panda:0000@localhost:3306/panda'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 로깅 설정
+    LOGGING_LEVEL = 'DEBUG'
