f78f1edd9e3a36c8acb0c4e4b1785c4726d355e5
Sang Min Park <minisstar@naver.com>
2024-05-20 11:19:57 +0900

    feat:받은정보 출력

--- app/park/Embedding/routes.py
+++ app/park/Embedding/routes.py
@@ -6,7 +6,13 @@ embedding_bp = Blueprint('embedding', __name__)
 @embedding_bp.route('/embedding', methods=['POST'])
 def embedding():
     try:
+        # JSON 데이터를 받아옴
         data = request.get_json()
+        
+        # 받은 데이터를 출력
+        print("Received data:", data)
+        
+        # 데이터에서 id 값을 추출
         embedding_id = data.get('id')
 
         # 임베딩 알고리즘 로직
