9b318154f1ebecabae8793778b87089a3a844e04
Sang Min Park <minisstar@naver.com>
2024-06-03 18:25:00 +0900

    feat: .디비 환경변수화.

--- app/common/config.py
+++ app/common/config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# .env 파일 로드
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://panda:0000@localhost:3306/panda'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # 로깅 설정
-    LOGGING_LEVEL = 'DEBUG'
+    LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'DEBUG')
